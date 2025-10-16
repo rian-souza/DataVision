3730693a86e2b1927cfbde30425394cabf5820d1
rian-souza <riansilva91@yahoo.com.br>
2025-10-15 21:21:05 -0300

    feat: adicionar função de validação de opções do usuário

--- datavision.py
+++ datavision.py
@@ -3,10 +3,21 @@ import os
 def limpar_terminal():
     os.system("cls" if os.name == "nt" else "clear")
 
+def validar(a, b):
+    while True:
+        try:
+            temp = int(input("Insira a opção desejada: "))
+            if a <= temp <= b:
+                return temp
+            else:
+                print(f"Insira uma opção entre {a} e {b}") 
+        except ValueError:
+            print("Digite somente números.")
+
 def menu_principal():
     while True:
         print("1 - Ler arquivo CSV.\n2 - Arquivos lidos\n0 - Sair.")
-        temp = int(input("Insira a opção desejada: "))
+        temp = validar(0, 2)
         if temp == 1:
             pass
         elif temp == 0:
